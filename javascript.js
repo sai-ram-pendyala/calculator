38bd5e07aba7887be9c83c03fbc327002eece022
sai-ram-pendyala <89296568+sai-ram-pendyala@users.noreply.github.com>
2023-02-27 17:32:47 +0530

    Add functionalities

---
+++ javascript.js
@@ -0,0 +1,49 @@
+let buttons = document.querySelectorAll('.btn');
+let display = document.querySelector('#input');
+let lastValue;
+let operator;
+
+function operate(e) {
+    let button = e.target;
+    let value = button.value;
+    // console.log(value);
+    // console.log(lastValue, operator, display.value);
+
+    if(value === '+' || value === '-' || value === '*' || value === '/') {
+        lastValue = display.value;
+        operator = value;
+        display.value = '';
+    }
+
+    else if(value === '=') {
+        let res = 0;
+        if(operator === '+') {
+            res = parseFloat(lastValue) + parseFloat(display.value);
+        }
+
+        else if(operator === '-') {
+            res = parseFloat(lastValue) - parseFloat(display.value);
+        }
+
+        else if(operator === '*') {
+            res = parseFloat(lastValue) * parseFloat(display.value);
+        }
+
+        else if(operator === '/') {
+            res = parseFloat(lastValue) / parseFloat(display.value);
+        }
+
+        lastValue = res;
+        display.value = res;
+    }
+
+    else if(value === 'C') {
+        display.value = '';
+    }
+
+    else {
+        display.value += value;
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', operate));
