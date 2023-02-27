de79d5ead98262f7e551f60e66803d8163535d7d
sai-ram-pendyala <89296568+sai-ram-pendyala@users.noreply.github.com>
2023-02-27 17:48:26 +0530

    Change functionality of operators

--- javascript.js
+++ javascript.js
@@ -1,51 +1,75 @@
 let buttons = document.querySelectorAll('.btn');
 let display = document.querySelector('#input');
-let lastValue;
-let operator;
+let lastValue = 0;
+let operator = '';
 
 function operate(e) {
     let button = e.target;
     let value = button.value;
-    // console.log(value);
-    // console.log(lastValue, operator, display.value);
-
+    console.log(value);
+    
     if(value === '+' || value === '-' || value === '*' || value === '/') {
-        lastValue = display.value;
-        operator = value;
-        display.value = '';
-    }
+        if(operator !== '') {
+            let res = 0;
+            if(operator === '+') {
+                res = parseFloat(lastValue) + parseFloat(display.value);
+            }
+            
+            else if(operator === '-') {
+                res = parseFloat(lastValue) - parseFloat(display.value);
+            }
 
+            else if(operator === '*') {
+                res = parseFloat(lastValue) * parseFloat(display.value);
+            }
+            
+            else if(operator === '/') {
+                res = parseFloat(lastValue) / parseFloat(display.value);
+            }
+            
+            lastValue = res;
+        }
+        
+        else {
+            lastValue = display.value;
+        }
+        
+        display.value = '';
+        operator = value;
+    }
+    
     else if(value === '=') {
         let res = 0;
         if(operator === '+') {
             res = parseFloat(lastValue) + parseFloat(display.value);
         }
-
+        
         else if(operator === '-') {
             res = parseFloat(lastValue) - parseFloat(display.value);
         }
-
+        
         else if(operator === '*') {
             res = parseFloat(lastValue) * parseFloat(display.value);
         }
-
+        
         else if(operator === '/') {
             res = parseFloat(lastValue) / parseFloat(display.value);
         }
-
+        
         lastValue = res;
         display.value = res;
     }
-
+    
     else if(value === 'C') {
         display.value = '';
         lastValue = '';
         operator = '';
     }
-
+    
     else {
         display.value += value;
     }
+    console.log(lastValue, operator, display.value);
 }
 
 buttons.forEach(button => button.addEventListener('click', operate));
